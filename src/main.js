6860bc3620d7ef8475976ec6d7febd41718c9e18
asd4a10 <amanzholov000@gmail.com>
2022-09-18 22:19:24 +0600

    fix

--- src/main.js
+++ src/main.js
@@ -27,15 +27,15 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
+// const firebaseApp = initializeApp(firebaseConfig);
+// const db = getFirestore(firebaseApp);
 
 // try {
 //   const docRef = await addDoc(collection(db, "users"), {
 //     first: "Ada",
 //     last: "Lovelace",
 //     born: 1815
-//   });
+// });
 //   console.log("Document written with ID: ", docRef.id);
 // } catch (e) {
 //   console.error("Error adding document: ", e);
@@ -53,11 +53,6 @@ const db = getFirestore(firebaseApp);
 // } catch (e) {
 //   console.error("Error adding document: ", e);
 // }
-
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
 
 const app = createApp(App);
 
